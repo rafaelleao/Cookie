5d0ea97587172f3f3ea07173a9945786251b3b5f
Rafael Leão <rafaeldeleao@gmail.com>
2021-03-27 21:34:53 +0100

    Initial commit

---
+++ Cookie/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Cookie
+//
+//  Created by Rafael Leao on 27.03.21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ Cookie/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cookie
+//
+//  Created by Rafael Leao on 27.03.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
