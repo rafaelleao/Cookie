7deeebd2427ec8c60fd07a930d3e3df41d8cddd7
Rafael Leão <rafaeldeleao@gmail.com>
2023-10-08 00:53:34 +0200

    Update test project

---
+++ Example/CookieTest/CookieTest/CookieTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookieTestApp.swift
+//  CookieTest
+//
+//  Created by Rafael Leão on 08.10.23.
+//
+
+import SwiftUI
+
+@main
+struct CookieTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "Cookie",
     platforms: [
-        .iOS(.v14)
+        .iOS(.v16)
     ],
     products: [
         .library(
